5fbfa2187f188060838057660c992e199a556765
Wedson <wquintanilhadasilva@gmail.com>
2024-07-23 20:19:13 -0300

    add resources

---
+++ aulas-state/state-show-replace-import-refresh/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_um" {
+    bucket = "wedsonsilva-terraform-commands-1"  
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+    bucket = "wedsonsilva-terraform-commands-2"  
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+    bucket = "wedsonsilva-terraform-commands-3"  
+}
