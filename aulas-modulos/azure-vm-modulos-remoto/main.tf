2df0cd84bebeeaa0b664b3fb967918fa6408f704
Wedson <wquintanilhadasilva@gmail.com>
2024-07-29 16:31:15 -0300

    formatação

--- aulas-modulos/azure-vm-modulos-remoto/main.tf
+++ aulas-modulos/azure-vm-modulos-remoto/main.tf
@@ -26,11 +26,11 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "5.2.0"
 
-  resource_group_name = azurerm_resource_group.resource_group.name
+  resource_group_name     = azurerm_resource_group.resource_group.name
   resource_group_location = var.location
-  use_for_each        = true
-  subnet_names        = ["subnet-${var.environment}"]
-  tags                = local.common_tags
-  vnet_name           = "vnet-${var.environment}"
+  use_for_each            = true
+  subnet_names            = ["subnet-${var.environment}"]
+  tags                    = local.common_tags
+  vnet_name               = "vnet-${var.environment}"
 
 }
