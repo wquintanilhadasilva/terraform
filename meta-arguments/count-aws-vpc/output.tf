6aa4007c56a5513cde51091ef8605fe560313dc5
Wedson <wquintanilhadasilva@gmail.com>
2024-07-30 15:01:40 -0300

    add count meta argument

---
+++ meta-arguments/count-aws-vpc/output.tf
@@ -0,0 +1,15 @@
+output "subnet_0_id" {
+  description = "ID da subnet criada na aws"
+  value       = aws_subnet.subnet[0].id
+}
+
+output "subnet_1_id" {
+  description = "ID da subnet criada na aws"
+  value       = aws_subnet.subnet[1].id
+}
+
+
+output "subnet_2_id" {
+  description = "ID da subnet criada na aws"
+  value       = aws_subnet.subnet[2].id
+}
